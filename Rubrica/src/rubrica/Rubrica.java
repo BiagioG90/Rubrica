6f49049547b45105fc80cdcdfac13c07afbcddd1
BiagioG90 federer.90@hotmail.it <BiagioG90 federer.90@hotmail.it>
2016-10-03 12:06:13 +0200

    Inizio Progetto Rubrica

---
+++ Rubrica/src/rubrica/Rubrica.java
@@ -0,0 +1,20 @@
+package rubrica;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Rubrica {
+
+	private Map<String, Voce> voci = new TreeMap<String, Voce>();
+	
+	public Voce aggiungiVoce(String nome, String cognome, String telefono) throws VoceGiaEsiste{
+		
+		if(voci.containsKey(nome + " " + cognome)){
+			throw new VoceGiaEsiste("era dentro classe rubrica metodo aggiungi");
+		}
+		
+		Voce v = new Voce(nome, cognome, telefono);
+		voci.put(nome + " " + cognome, v);
+		return v;
+	}
+}
