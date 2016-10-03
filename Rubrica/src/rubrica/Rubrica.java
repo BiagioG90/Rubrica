e5a82722143f7d26ec51f41419a61ea22abd56b5
marti90 <debernardi.martina@gmail.com>
2016-10-03 15:45:53 +0200

    Scrivo una variabile

--- Rubrica/src/rubrica/Rubrica.java
+++ Rubrica/src/rubrica/Rubrica.java
@@ -6,7 +6,7 @@ import java.util.TreeMap;
 public class Rubrica {
 
 	private Map<String, Voce> voci = new TreeMap<String, Voce>();
-	//ciao
+	private int a=0;
 	//primo metodo
 	//altro commento
 	//altro commento
