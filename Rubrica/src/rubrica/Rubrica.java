e81e734f111f1461757d55ad9c3074ea99957326
marti90 <debernardi.martina@gmail.com>
2016-10-04 11:14:23 +0200

    Aggiungo classe per eccezione

--- Rubrica/src/rubrica/Rubrica.java
+++ Rubrica/src/rubrica/Rubrica.java
@@ -23,4 +23,5 @@ public class Rubrica {
 		voci.put(nome + " " + cognome, v);
 		return v;
 	}
+	
 }
---
+++ Rubrica/src/rubrica/UtenteNonEsiste.java
@@ -0,0 +1,16 @@
+package rubrica;
+
+public class UtenteNonEsiste extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public UtenteNonEsiste(String s){
+		super(s);
+	}
+	
+	
+
+}
